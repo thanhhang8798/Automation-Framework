7456e5d8e649a3fdd727beb51e651d77430a810e
hang.btt <thanhhang8798@gmail.com>
2025-09-18 09:31:28 +0700

    cookies update

---
+++ src/test/java/nopcommerce/Common_Login.java
@@ -0,0 +1,77 @@
+package nopcommerce;
+
+import core.BaseTest;
+import core.GlobalConstants;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pageObjects.PageGenerator;
+import pageObjects.nopCommerce.admin.AdminDashboardPO;
+import pageObjects.nopCommerce.admin.AdminLoginPO;
+import pageObjects.nopCommerce.user.UserHomePageObject;
+import pageObjects.nopCommerce.user.UserLoginPageObject;
+import pageObjects.nopCommerce.user.UserMyAccountSideBarPO.*;
+import pageObjects.nopCommerce.user.UserRegisterPageObject;
+
+import java.util.Set;
+
+public class Common_Login extends BaseTest{
+    @Parameters({"userUrl", "adminUrl", "browser"})
+    @BeforeTest
+    public void beforeClass(String userUrl, String adminUrl, String browserName) {
+        this.userUrl = userUrl;
+        this.adminUrl = adminUrl;
+
+        driver = getBrowserDriver(userUrl, browserName);
+        userHomePage = PageGenerator.getPage(UserHomePageObject.class, driver);
+
+        firstName = "Bui";
+        lastName = "Hang";
+        userEmail = "buihang" + getRandomNumber() + "@gmail.com";
+        userPassword = "Auto222@@@";
+        confirmPassword = "Auto222@@@";
+
+        userRegisterPage = userHomePage.openRegisterPage(driver);
+        userRegisterPage.enterToTextboxByID(driver,"FirstName", firstName);
+        userRegisterPage.enterToTextboxByID(driver,"LastName", lastName);
+        userRegisterPage.enterToTextboxByID(driver,"Email", userEmail);
+        userRegisterPage.clickToCheckboxByID(driver, "Newsletter");
+        userRegisterPage.enterToTextboxByID(driver,"Password", userPassword);
+        userRegisterPage.enterToTextboxByID(driver,"ConfirmPassword", confirmPassword);
+        userRegisterPage.clickToButtonByText(driver, "Register");
+
+        Assert.assertEquals(userRegisterPage.getRegisterResultMassage(), "Your registration completed");
+
+        userRegisterPage.clickToLinkByText(driver, "Log out");;
+        userHomePage = PageGenerator.getPage(UserHomePageObject.class, driver);
+
+        userLoginPage = userHomePage.clickToLoginLinkAtUserSite(driver);
+        userLoginPage.enterToTextboxByID(driver,"Email", userEmail);
+        userLoginPage.enterToTextboxByID(driver,"Password", userPassword);
+        userLoginPage.clickToButtonByText(driver, "Log in");
+
+        userHomePage = PageGenerator.getPage(UserHomePageObject.class, driver);
+
+        // get cookies
+        nopCommerceCookies = userHomePage.getAllCookies(driver);
+        System.out.println(nopCommerceCookies);
+
+        driver.quit();
+    }
+
+
+    private WebDriver driver;
+    private String userUrl, adminUrl;
+    String firstName, lastName, confirmPassword;
+    public static String userEmail, userPassword;
+
+    private UserHomePageObject userHomePage;
+    private UserRegisterPageObject userRegisterPage;
+    private UserLoginPageObject userLoginPage;
+    public static Set<Cookie> nopCommerceCookies;
+}
+
+
+
+
---
+++ src/test/java/nopcommerce/Topic_09_Share_State.java
@@ -0,0 +1,77 @@
+package nopcommerce;
+
+import core.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.devtools.v134.page.Page;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageObjects.PageGenerator;
+import pageObjects.nopCommerce.admin.AdminDashboardPO;
+import pageObjects.nopCommerce.admin.AdminLoginPO;
+import pageObjects.nopCommerce.user.UserHomePageObject;
+import pageObjects.nopCommerce.user.UserLoginPageObject;
+import pageObjects.nopCommerce.user.UserMyAccountSideBarPO.*;
+import pageObjects.nopCommerce.user.UserRegisterPageObject;
+
+public class Topic_09_Share_State extends BaseTest{
+    @Parameters({"userUrl", "adminUrl", "browser"})
+    @BeforeClass
+    public void beforeClass(String userUrl, String adminUrl, String browserName) {
+        this.userUrl = userUrl;
+        this.adminUrl = adminUrl;
+
+        driver = getBrowserDriver(userUrl, browserName);
+        userHomePage = PageGenerator.getPage(UserHomePageObject.class, driver);
+        userHomePage.clickToLinkByText(driver, "Log in");
+
+        userLoginPage = PageGenerator.getPage(UserLoginPageObject.class, driver);
+        userLoginPage.enterToTextboxByID(driver, "Email", Common_Login.userEmail);
+        userLoginPage.enterToTextboxByID(driver, "Password", Common_Login.userPassword);
+        userLoginPage.clickToButtonByText(driver, "Log in");
+
+        userHomePage = PageGenerator.getPage(UserHomePageObject.class, driver);
+    }
+
+    @Test
+    public void TC_01_MyAccount() {
+        userHomePage.clickToLinkByText(driver, "My account");
+        userCustomerInforPage = PageGenerator.getPage(UserCustomerInforPO.class, driver);
+        userCustomerInforPage.sleepInSecond(3);
+
+        Assert.assertTrue(userCustomerInforPage.isCheckboxSelectedByID(driver, "Newsletter"));
+    }
+
+//    @Test
+    public void TC_02_Dynamic_Locator() {
+        userOrdersPage = userCustomerInforPage.openUserOrderPage();
+        userBackInStockSubscriptionsPage = userOrdersPage.openUserBackInStockPage();
+        userAddressesPage = userBackInStockSubscriptionsPage.openUserAddressesPage();
+        userDownloadableProductsPage = userAddressesPage.openUserDownloadableProductPage();
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    private WebDriver driver;
+    private String userUrl, adminUrl;
+    String firstName, lastName, userEmail, userPassword, confirmPassword;
+
+    private UserHomePageObject userHomePage;
+    private UserRegisterPageObject userRegisterPage;
+    private UserCustomerInforPO userCustomerInforPage;
+    private UserLoginPageObject userLoginPage;
+    private UserOrdersPO userOrdersPage;
+    private UserBackInStockSubscriptionsPO userBackInStockSubscriptionsPage;
+    private UserDownloadableProductsPO userDownloadableProductsPage;
+    private UserAddressesPO userAddressesPage;
+
+    private AdminDashboardPO adminDashbpardPage;
+    private AdminLoginPO adminLoginPage;
+}
+
